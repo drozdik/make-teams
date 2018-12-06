74f432170850690b02b096e15d7cdcc9ecde0824
vladimir <v_drozdik@hotmail.com>
2018-12-06 21:48:38 +0200

    Add removing team method

--- src/main/java/maketeams/Teams.java
+++ src/main/java/maketeams/Teams.java
@@ -1,10 +1,25 @@
 package maketeams;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 public class Teams {
+
+    private List<Team> teams = new ArrayList();
+
     public Collection<String> getAllTeamNames() {
-        return Collections.emptyList();
+        return teams.stream().map(team -> team.getName()).collect(toList());
+    }
+
+    public void addTeam(Team team) {
+        teams.add(team);
+    }
+
+    public void removeTeam(Team team) {
+        teams.remove(team);
     }
 }
--- src/test/java/maketeams/TeamsTest.java
+++ src/test/java/maketeams/TeamsTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import java.util.Collection;
 
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.*;
 
 public class TeamsTest {
@@ -18,5 +20,35 @@ public class TeamsTest {
         Collection<String> teamNames = teams.getAllTeamNames();
         // then
         assertThat(teamNames, empty());
+    }
+
+    @Test
+    public void shouldReturnAddedTeamName() throws Exception {
+        // given
+        Teams teams = new Teams();
+        Team teamA = new Team("teamA");
+        // when
+        teams.addTeam(teamA);
+        Collection<String> teamNames = teams.getAllTeamNames();
+        // then
+        assertThat(teamNames, hasItem("teamA"));
+    }
+
+    @Test
+    public void shouldNotReturnDeletedTeamName() throws Exception {
+        // given
+        Teams teams = new Teams();
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+
+        // when
+        teams.addTeam(teamA);
+        teams.addTeam(teamB);
+        teams.removeTeam(teamA);
+        Collection<String> teamNames = teams.getAllTeamNames();
+
+        // then
+        assertThat(teamNames, not(hasItem("teamA")));
+        assertThat(teamNames, hasItem("teamB"));
     }
 }
